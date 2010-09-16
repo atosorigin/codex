ffe079fedd44d58eb671b82471a51d53eb802d51
simondelliott <simondelliott@gmail.com>
2010-09-16 08:59:22 +0100

    tidied up the index added a style sheet

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,40 @@ ActionController::Routing::Routes.draw do |map|
   map.users     '/users',                   :controller => 'users', :action => 'index'
   map.connect   '/users/:user_name',        :controller => 'users', :action => 'show',  :user_name => /[a-zA-Z0-9 _]+/
 
+  map.parcels       '/users/:user_name/orders/:order_id/consignments/:consignment_id/parcels',
+     :controller => 'parcels',
+     :action => 'index',
+     :user_name => /[a-zA-Z0-9 _]+/,
+     :order_id => /[0-9]+/,
+     :consignment_id => /[a-zA-Z0-9 _]+/
+  map.connect       '/users/:user_name/orders/:order_id/consignments/:consignment_id/parcels/:parcel_id',
+     :controller => 'parcels',
+     :action => 'show',
+     :user_name => /[a-zA-Z0-9 _]+/,
+     :order_id => /[0-9]+/,
+     :consignment_id => /[a-zA-Z0-9 _]+/,
+     :parcel_id => /[a-zA-Z0-9 _]+/
+  map.connect       '/users/:user_name/orders/:order_id/consignments/:consignment_id/parcels/:parcel_id/label',
+     :controller => 'parcels',
+     :action => 'label',
+     :user_name => /[a-zA-Z0-9 _]+/,
+     :order_id => /[0-9]+/,
+     :consignment_id => /[a-zA-Z0-9 _]+/,
+     :parcel_id => /[a-zA-Z0-9 _]+/
+
+
+  map.consignments   '/users/:user_name/orders/:order_id/consignments',
+     :controller => 'consignments',
+     :action => 'index',
+     :user_name => /[a-zA-Z0-9 _]+/,
+     :order_id => /[0-9]+/
+  map.connect       '/users/:user_name/orders/:order_id/consignments/:consignment_id',
+     :controller => 'consignments',
+     :action => 'show',
+     :user_name => /[a-zA-Z0-9 _]+/,
+     :order_id => /[0-9]+/,
+     :consignment_id => /[a-zA-Z0-9 _]+/
+
   map.orders    '/users/:user_name/orders',             :controller => 'orders', :action => 'index',  :user_name => /[a-zA-Z0-9 _]+/
   map.connect   '/users/:user_name/orders/:order_id',   :controller => 'orders', :action => 'show',   :user_name => /[a-zA-Z0-9 _]+/,   :order_id => /[0-9]+/
 
