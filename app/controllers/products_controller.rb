0869f2bbdcdbbe69d6921340c76e3a6c56809c72
simondelliott <simondelliott@gmail.com>
2010-10-06 12:10:01 +0100

    tidied up the 404 kludge

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,10 @@ class ProductsController < ApplicationController
     @gazateer_product = YodelGazetteer.new().get_product_information_by_description(@product.name)
     if @gazateer_product.nil?
       #render :text => "This should be a 404", :status => 404
-      render :file => 'layouts/404.html.erb', :status => 404, :layout => true
+
+      flash.now[:error] = "the product you are looking for <strong>/products/" + @product.name + "</strong>" +
+        " does not exist, please look at <a href='/products'>/products</a>"
+      render :file => 'shared/404.html.erb', :status => 404, :layout => true
 
     else
       render :file => "products/show.xml.erb"
