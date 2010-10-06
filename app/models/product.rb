0e3f5b49bc4fbe4cb5112440b69f96adbcf63cce
simondelliott <simondelliott@gmail.com>
2010-10-06 11:04:55 +0100

    added attributes to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ActiveRecord::Base
 
   attr_accessor :name
+  attr_accessor :product_code
+  attr_accessor :feature_code
+  attr_accessor :product_line_1
+  attr_accessor :product_line_2
+  attr_accessor :service_description
+  attr_accessor :handling_feature_text
+  attr_accessor :date_code
+  attr_accessor :day_text
+  attr_accessor :time_code
+  attr_accessor :time_text
+  attr_accessor :handling_feature_text
+  attr_accessor :file_type
 
 end
